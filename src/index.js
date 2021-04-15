79a49db39550a17d1971b771836b011e5b0eaa90
danvarga1985 <danvarga1985@gmail.com>
2021-04-15 14:50:32 +0200

    Webpack bundling all the app code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-alert("Hello from WEBPACK")
+import { run } from "./app/app";
+import { ComponentService } from "./app/component.service";
+import { AlertService } from "./app/alert.service";
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+run(alertService, componentService);
