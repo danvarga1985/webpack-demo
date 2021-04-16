2c9695cb95f5360d45b8f4d91163f0444f5909ea
danvarga1985 <danvarga1985@gmail.com>
2021-04-16 11:12:25 +0200

    Add html-loader, asset modules, clean webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -3,19 +3,31 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     module: {
-        rules: [{
-            test: /\.scss$/,
-            // Order is important! Processed in reverse order -> transform css, then inject it into the dom
-            use: [
-                "style-loader", // 3. Inject styles into DOM
-                "css-loader", // 2. Turns css into common javascript
-                "sass-loader", // 1. Turn sass into css
-            ],
-        }]
+        rules: [
+            {
+                test: /\.scss$/,
+                // Order is important! Processed in reverse order -> transform css, then inject it into the dom
+                use: [
+                    "style-loader", // 3. Inject styles into DOM
+                    "css-loader", // 2. Turns css into common javascript
+                    "sass-loader", // 1. Turn sass into css
+                ],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    "html-loader"
+                ]
+            },
+            {
+                test: /\.(svg|png|jpe?g|gif)$/i,
+                type: 'asset/resource'
+            }
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
-        })
+        }),
     ],
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Provides the CleanWebpack plugin
 const common = require("./webpack.common"); // Provides the contents of "webpack.common.js"
 const { merge }  = require("webpack-merge"); // Provides the "webpack merge plugin"
 
@@ -8,4 +9,7 @@ module.exports = merge(common, {
         filename: "main.[contenthash].js",
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
 });
